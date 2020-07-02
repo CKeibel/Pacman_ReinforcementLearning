c5569cdda55e9a4ecc86ac77783edd02f5df3fa9
Christopher Keibel <christopher.keibel.90@gmail.com>
2020-07-02 16:49:07 +0200

    Pacman CNN

---
+++ DQN/convolutional_layers/PacmanAgent.py
@@ -0,0 +1,209 @@
+import argparse
+
+import gym
+from gym import wrappers, logger
+
+# my imports
+from dqn import *
+import collections
+import numpy as np
+import random
+import pandas as pd
+from PIL import Image, ImageDraw
+
+
+
+
+
+# TODO Die gewünschte Anzahl an zu spielenden Spielen
+episode_count = 100
+
+# TODO Verzeichnis für die Ergebnisse der Spiele
+# Default unter gym/results
+outdir = './results'
+
+
+
+class PacmanAgent(object):
+
+    def __init__(self, env):
+        # env variables
+        self.env = env
+        self.actions = self.env.action_space.n
+        self.img_shape = (84, 84, 3) # pixels, pixels, rgb
+
+        # DQN Agent Variables
+        self.replay_buffer_size = 60000
+        self.train_start = 3000
+        self.memory = collections.deque(maxlen=self.replay_buffer_size)
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_steps = 100000
+        self.epsilon_step = (self.epsilon - self.epsilon_min) / self.epsilon_steps        
+        # DQN Network Variables
+        self.lr = 0.0003
+        self.model = DQN(self.img_shape, self.actions, self.lr)
+        self.target_model = DQN(self.img_shape, self.actions, self.lr)
+        self.target_model.update_model(self.model)
+        self.batch_size = 32
+        self.sync_models = 10000
+        self.path_model = "pacman_model.h5"
+        self.path_target_model = "pacman_targetmodel.h5"
+        self.load = False
+        if self.load:
+            self.model.load_model(self.path_model)
+            self.target_model.load_model(self.path_target_model)
+
+
+    def act(self, state, flag):
+        
+        action_int = 0
+        
+        # flag unterscheidet ob im Trainingsmodus(TRUE) oder Playmodus(FALSE)
+        if flag:
+            # Epsilon Greedy
+            if np.random.rand() <= self.epsilon:
+                action_int = self.env.action_space.sample() # Exploration
+            else:
+                action_int = np.argmax(self.model.predict(state)) # Exploitation
+        else:
+            action_int = np.argmax(self.model.predict(state))
+              
+        
+        if action_int == 0:
+            action = "GO_NORTH"
+        elif action_int == 1:
+            action = "GO_WEST"
+        elif action_int == 2:
+            action = "GO_EAST"
+        elif action_int == 3:
+            action = "GO_SOUTH"
+        elif action_int == 4:
+            action = "QUIT_GAME"
+        elif action_int == 5:
+            action = "WAIT"
+        
+        return action, action_int
+
+
+    def train(self, num_episodes):
+        total_rewards = []
+        it = 0
+
+        for episode in range(num_episodes):
+            total_reward = 0.0
+            state = self.env.reset()
+
+            while True:
+                it += 1
+                action, action_int = self.act(state, True)
+                next_state, reward, done, _ = self.env.step(action)
+                total_reward += reward
+
+                if done and total_reward < 16:
+                    reward = -100
+
+                self.remember(state, action_int, reward, next_state, done)
+                self.epsilon_anneal()
+                self.replay()
+                state = next_state
+
+                if it % self.sync_models == 0:
+                    self.target_model.update_model(self.model)
+
+                if done:
+                    total_rewards.append(total_reward)
+                    mean_reward = np.mean(total_rewards[-5:])
+                    df = pd.DataFrame([total_reward], columns=['Rewards'])
+                    df.to_csv("total_rewards.csv", index=False, mode="a", header="False")
+
+                    print("Episode:", episode+1, "\tMemSize:", len(self.memory), "\tReward:", total_reward, "\tMean:", mean_reward)
+                    self.model.save_model(self.path_model)
+                    self.target_model.save_model(self.path_target_model)
+                    break
+            
+    def epsilon_anneal(self):
+        if len(self.memory) < self.train_start:
+            return
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_step
+
+    def remember(self, state, action_int, reward, next_state, done):
+        self.memory.append([state, action_int, reward, next_state, done])
+
+    def replay(self):
+        if len(self.memory) < self.train_start:
+            return
+
+        minibatch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, states_next, dones = zip(*minibatch)
+
+        states = np.concatenate(states)
+        states_next = np.concatenate(states_next)
+
+        q_values = self.model.predict(states)
+        q_values_next = self.target_model.predict(states_next)
+
+        for i in range(self.batch_size):
+            a = actions[i]
+            done = dones[i]
+            if done:
+                q_values[i][a] = rewards[i]
+            else:
+                q_values[i][a] = rewards[i] + self.gamma * np.max(q_values_next[i])
+
+        self.model.train(states, q_values)
+
+    def play(self, num_episodes):
+        self.model.load_model(self.path_model)
+        self.target_model.load_model(self.path_target_model)
+
+        for episode in range(num_episodes):
+            state = self.env.reset()
+
+            while True:
+                action = self.act(state, False)
+                state, _, done, _ = self.env.step(action)                
+                if done:
+                    break
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('env_id', nargs='?', default='gym_pacman_environment:test-v0', help='Select the environment to run')
+    args = parser.parse_args()
+
+    # Hier könnt ihr das Level auf logger.DEBUG or logger.WARN setzen, um mehr Details auszugeben
+    logger.set_level(logger.INFO)
+
+    env = gym.make(args.env_id)
+
+    if outdir is None:
+        raise Exception("Please set the directory where to put the results into")
+    
+    env = wrappers.Monitor(env, directory=outdir, force=True)
+    env.seed(0)
+    agent = PacmanAgent(env)
+    agent.train(num_episodes=1)
+    agent.model.save_model('pacman_10000_runs.h5')
+    
+       
+
+    # for i in range(1):
+    #     state = env.reset()
+    #     total_reward = 0
+    #     while True:
+    #         action = agent.act(state)
+    #         state, reward, done, debug = env.step(action)
+    #         #print(state)
+    #         print(reward)
+    #         total_reward += reward
+    #         if done:
+    #             break
+    #     print(total_reward)
+    
+    env.close()
+    
+
---
+++ DQN/convolutional_layers/PacmanEnv.py
@@ -0,0 +1,292 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+import numpy as np
+
+import socket as skt
+import threading
+import json
+from builtins import print
+from struct import unpack
+
+from PIL import Image, ImageDraw
+
+
+socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+buffer = []
+
+
+def hasNext():
+    return len(buffer) > 0
+
+
+def nextString():
+    return buffer.pop(0)
+
+
+class BGThread(threading.Thread):
+    def run(self):
+        print('Client started\n')
+        try:
+            socket.connect(('localhost', 6000))
+        except skt.error:
+            socket.close()
+            print('connecting failed')
+            return
+        print('connected')
+        while True:
+            try:
+                threading.Event().wait(0.001)
+
+                data = read()
+                if data == 'SERVER_STOPPED':
+                    break
+                if not (str.startswith(data, 'PREPARING_GAME')):
+                    buffer.append(data)
+            except Exception as e:
+                print('terminated BG-Thread')
+                raise e
+        print('terminated BG-Thread')
+
+
+def read():
+    size = 0
+    try:
+        data = socket.recv(1)
+        if len(data) == 0:
+            raise Exception("Connection lost")
+        # unpack the received data from bytes to integer
+        packages = unpack('b', data)[0]
+        # return the packages to char and cast it to int
+        packages = int(chr(packages))
+        # print("packages: "+str(packages))
+
+        # print("packages "+str(packages))
+        completeData = ""
+        for i in range(0, packages):
+            data = socket.recv(1)
+            # unpack the received data from bytes to integer
+            size = unpack('b', data)[0]
+            # return the size to char and cast it to int to be used for pow
+            size = int(chr(size))
+            size = pow(2, size)
+
+            data = socket.recv(size).decode('UTF-8')
+
+            rawData = repr(data)
+            if i < packages - 1:
+                procData = rawData[1:len(rawData) - 1]  # without the leading and trailing '
+            else:
+                procData = ""
+                i = 0
+                # print("rawData: "+str(rawData))
+                while True:
+                    procData += rawData[i]
+                    # print(procData)
+                    i = i + 1
+                    if procData.endswith('\\x00'):
+                        procData = procData[0:len(procData) - 4]
+                        break
+                    if i == len(rawData):
+                        break
+                procData = procData[1:len(procData)]  # without the leading '
+
+            completeData += procData
+
+        return completeData
+    except OverflowError as e:
+        print(e)
+        print('Received ' + str(size) + ' bytes')
+        return ''
+
+
+def write(data):
+    paddedLength = findBest2power(len(data))
+
+    socket.sendall(str(paddedLength).encode('UTF-8'))
+
+    paddedLength = pow(2, paddedLength)
+
+    paddedBytesToWrite = str('')
+    for b in range(0, paddedLength):
+        paddedBytesToWrite = paddedBytesToWrite + ' '
+
+    paddedBytesToWrite = data + paddedBytesToWrite[len(data): paddedLength]
+
+    socket.sendall(str(paddedBytesToWrite).encode('UTF-8'))
+
+
+def findBest2power(length):
+    i = 7
+    while length >= pow(2, i):
+        i += 1
+    return i
+
+
+def writeAgentConfig():
+    name = "MyPythonAgent"
+    clazz = "de.fh.pacman.Pacman"
+    agentConfig = "{\n\"class\":AgentConfig\n\"name\":" + name + "\n\"entityClass\":" + clazz + "\n}"
+    write(agentConfig)
+
+
+class PacmanEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    
+    self.action_space = spaces.Discrete(4)#up, left, right, down
+    self.observation_space = spaces.box.Box(low=0, high=255, shape=(84, 84, 3), dtype=np.uint8)
+
+    self.actionEffect = ""
+    self.percept = None
+    self.totalDots = -1
+    self.reward_dots = 16
+    self.tmp_dots = 16
+    self.steps = 0
+
+    
+    t = BGThread()
+    t.start()
+    threading.Event().wait(0.01)
+
+  def countDots(self, view):
+      dots = 0
+      for x in range(0, len(view)):
+          for y in range(0, len(view[x])):
+              t = view[x][y]
+              if view[x][y] == "D":
+                  dots = dots+1
+      return dots
+
+  def step(self, action):
+    # reward
+    self.steps += 1
+    
+    if self.percept is None:
+        reward = 0
+    elif self.steps > 4:
+        self.tmp_dots = self.countDots(self.percept['view'])
+        if self.reward_dots > self.tmp_dots:   
+            reward = 1
+            self.reward_dots = self.tmp_dots
+        else:
+            reward = 0
+    else: 
+        reward = 0
+
+
+        
+
+    # observation
+    if self.percept is None:
+        ob = [['E' for _ in range(7)] for _ in range(7)]
+    else:
+        ob = self.percept['view']
+
+    ob = self.draw_state(ob)
+
+
+        
+    
+    while not hasNext():
+        threading.Event().wait(0.001)
+    data = nextString()
+    js = json.loads(data, encoding="UTF-8")
+    # print("js: "+str(js))
+
+    # Differ between received serverSignals, percepts and actionEffects
+    done = False
+    if isinstance(js, list):
+        pass
+    elif isinstance(js, dict):
+        if js["class"] == "PacmanActionEffect":
+            pass
+        elif js["class"] == "PacmanPercept":
+            self.percept = js
+            if self.totalDots == -1:
+                self.totalDots = self.countDots(js["view"])
+            write('\"'+action+'\"')
+        elif js["class"] == "PacmanGameResult":
+            done = True
+            pass
+    
+    return ob, reward, done, {}
+  
+  def reset(self):
+    # resseting
+    self.reward_dots = self.tmp_dots = 16
+    self.steps = 0
+    ob = [['E' for _ in range(7)] for _ in range(7)]
+    ob = self.draw_state(ob)
+
+
+    if self.actionEffect == "" :
+        writeAgentConfig()
+        self.actionEffect = "GAME_INITIALIZED"
+        while True:
+            while not hasNext():
+                threading.Event().wait(0.001)
+            data = nextString()
+            if data == '["STARTING_GAME"]':
+                break
+    print("Resetted")
+    
+    return ob ################################################################ Inital State je nach Map anpassen
+  
+  def render(self, mode='human'):
+    ...
+  def close(self):
+    ...
+
+  def index_to_ints(self, index):
+    str1 = index
+    x = None
+    y = None
+
+    try:
+        x = int(str1[1] + str1[2])
+    except:
+        try: 
+            x = int(str1[1])
+        except:
+            x = 1
+
+    try: 
+        y = int(str1[5] + str1[6])
+    except:
+        try:
+            y = int(str1[4] + str1[5])   
+        except:
+            try:
+                y = int(str1[4])
+            except:
+                try:
+                    y = int(str1[5])
+                except:
+                    y = 1
+
+
+    return x, y
+
+  def draw_state(self, state):
+    im = Image.new("RGB", (len(state)*12, len(state[0])*12))
+    draw = ImageDraw.Draw(im)
+
+    for x in range(0, len(state)):
+        for y in range(0, len(state[x])):
+            if state[y][x] == 'W':
+                draw.rectangle((x*12, y*12, x*12+12, y*12+12), fill=(0, 0, 139))
+            elif state[y][x] == 'P':
+                draw.rectangle((x*12, y*12, x*12+12, y*12+12), fill=(255, 255 ,0))
+            elif state[y][x] == 'G' or state[x][y] == 'GD':
+                draw.rectangle((x*12, y*12, x*12+12, y*12+12), fill=(255, 0, 0))
+            elif state[y][x] == 'D':
+                draw.rectangle((x*12, y*12, x*12+12, y*12+12), fill=(255, 255, 255))
+            elif state[y][x] == 'E':
+                draw.rectangle((x*12, y*12, x*12+12, y*12+12), fill=(0, 0, 0))
+    #im.show()
+    im = np.array(im)
+        
+    return im
+
